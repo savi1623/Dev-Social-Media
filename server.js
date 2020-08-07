75879f12c6ef36630f4fb7fff6350b62b9750ab0
Savi Williams <savannah.williams1623@gmail.com>
2020-08-07 11:14:39 -0600

    user routes updated

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const connectDB = require('./config/db.js');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
